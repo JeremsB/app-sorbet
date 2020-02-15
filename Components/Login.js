9ecdce88ba74af0c010f39368c817cd1b6f9e8f1
Jérémy <jerem.blcn@gmail.com>
2020-02-15 16:36:35 +0100

    Login screen

---
+++ Components/Login.js
@@ -0,0 +1,106 @@
+// Components/Login.js
+import React from 'react'
+import { StyleSheet, View, TextInput, Button, Text, Image } from 'react-native'
+
+class Login extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.mail = ""
+        this.password = ""
+    }
+
+    _mailTextInputChanged(text) {
+        this.mail = text // Modification du texte recherché à chaque saisie de texte, sans passer par le setState
+    }
+
+    _passwordTextInputChanged(text) {
+        this.password = text
+    }
+
+    _login() {
+        console.log("Hello test")
+    }
+
+    render() {
+        return (
+            <View style={styles.main_container}>
+                <Image
+                    style={{marginTop: 10,
+                        flex: 1,
+                        width: null,
+                        height: null,
+                        resizeMode: 'contain'}}
+                    source={require('../content/img/title.png')}
+                />
+                <Image
+                    style={{flex: 1,
+                        paddingBottom: 50,
+                        width: null,
+                        height: null,
+                        resizeMode: 'contain'}}
+                    source={require('../content/img/logo.png')}
+                />
+                <TextInput
+                    style={styles.textinputMail}
+                    placeholder='Email'
+                    onChangeText={(text) => this._mailTextInputChanged(text)}
+                />
+                <TextInput
+                    style={styles.textinputPwd}
+                    placeholder='Password'
+                    onChangeText={(text) => this._passwordTextInputChanged(text)}
+                />
+                <Button title='Login'
+                        color="pink"
+                        type='outline'
+                        onPress={() => this._login()}/>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    main_container: {
+        flex: 1,
+        marginLeft: 30,
+        marginRight: 30,
+        justifyContent: 'center',
+        paddingBottom: 50
+    },
+    textinputMail: {
+        height: 50,
+        width: 250,
+        borderColor: 'pink',
+        borderWidth: 2,
+        borderRadius: 5,
+        paddingLeft: 5,
+        marginBottom: 0,
+        marginTop: 100,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        color: 'pink'
+    },
+    textinputPwd: {
+        height: 50,
+        width: 250,
+        borderColor: 'pink',
+        borderWidth: 2,
+        borderRadius: 5,
+        paddingLeft: 5,
+        marginBottom: 20,
+        marginTop: 10,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        color: 'pink'
+    },
+    titleText: {
+        fontSize: 50,
+        color: 'pink',
+        textAlign: 'center',
+        marginBottom: 20
+    }
+})
+
+export default Login
+
