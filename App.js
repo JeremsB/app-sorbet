2e4fad464f6e283e31bf21b9ce7eb4eb67a257e2
Jerem <jerem.blcn@gmail.com>
2020-04-27 22:17:32 +0200

    integration de firebase

--- App.js
+++ App.js
@@ -3,7 +3,20 @@ import Navigation from './Navigation/Navigation'
 import { Provider } from 'react-redux'
 import Store from './Store/configureStore'
 import { StatusBar } from 'react-native'
+import * as firebase from 'firebase';
 
+const firebaseConfig = {
+    apiKey: "AIzaSyApEbLerHRgci8Po3zeySPNVZr0dtNKO4E",
+    authDomain: "sorbet-6a666.firebaseapp.com",
+    databaseURL: "https://sorbet-6a666.firebaseio.com",
+    projectId: "sorbet-6a666",
+    storageBucket: "sorbet-6a666.appspot.com",
+    messagingSenderId: "614640091551",
+    appId: "1:614640091551:web:c053d184bfbb5a7940bdaa",
+    measurementId: "G-W879WN4BMZ"
+}
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return (
